8eb563c63dcd7e5bc07168be21bea5d1325d9285
Jeffrey Akanama <akanamajeffrey@gmail.com>
2026-02-12 00:59:31 +0100

    Add restaurant website files

---
+++ script.js
@@ -0,0 +1,8 @@
+function orderNow() {
+  const name = prompt("What is your name?");
+  if (name) {
+    alert("Thank you " + name + "! We’ll contact you shortly 😊");
+  } else {
+    alert("Thank you for choosing us!");
+  }
+}
